0800f13d99940e21555a2fdc071dda907fc40503
Solomon APPIER-SIGN <braasig@gmail.com>
2020-02-10 11:55:33 +0000

    added method to get user's current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+void getLocation() async {
+  Position position = await Geolocator()
+      .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+  print(position);
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
